72868a5c71cd26f32906c1b5cbbef2343a7caa49
Aaron Peña <l.aarongalvan@gmail.com>
2024-03-24 21:44:57 -0500

    Consumo de API

---
+++ src/Components/CharacterList.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import Character from './Character';
+
+function CharacterList() {
+
+    const [characters, setCharacters] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch("https://rickandmortyapi.com/api/character?page=2");
+            const data = await response.json();
+            setLoading(false)
+            setCharacters(data.results);
+        }
+
+        fetchData()
+    }, []);
+
+
+    return (
+        <div className='container'>
+            {loading ? (
+                <h1>Loading...</h1>
+            ) : (
+                <div className='row'>
+                    {characters.map((character) => {
+                        return (
+                            <div className='col-md-4' key={character.id}>
+                                <Character character={character} />
+                            </div>
+                        );
+                    })}
+                </div>
+            )}
+
+        </div>
+    );
+}
+
+export default CharacterList
